8708e86da5c2dc897c7e3e3f1581e589af6b1f3d
DMachad0 <49269723+DMachad0@users.noreply.github.com>
2021-10-13 16:20:25 +0100

    titulo + favicon + lang

--- resources/views/register.blade.php
+++ resources/views/register.blade.php
@@ -1,13 +1,15 @@
 <!DOCTYPE html>
-<html lang="en" class="coming-soon">
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
 <head>
     <meta charset="utf-8">
-    <title>Registration Form</title>
+    <title>Gestão de Condomínios</title>
     <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
     <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0, user-scalable=no">
     <meta name="apple-mobile-web-app-capable" content="yes">
     <meta name="apple-touch-fullscreen" content="yes">
-    <meta name="author" content="KaijuThemes">
+    <meta name="author" content="Diogo Machado e Rui Alves">
+
+    <link rel="shortcut icon" href="{{ url('favicon.png') }}">
 
     <link type='text/css' href='http://fonts.googleapis.com/css?family=Roboto:300,400,400italic,500' rel='stylesheet'>
     <link type='text/css'  href="https://fonts.googleapis.com/icon?family=Material+Icons"  rel="stylesheet"> 
