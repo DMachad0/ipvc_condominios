78bd2a768af87eaec660daf1e0cee6df313a9248
Ciclista26 <ruijorge02012001@gmail.com>
2021-10-13 11:33:45 +0100

    favicon

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -4,8 +4,9 @@
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        <title>Laravel</title>
+        <title>Gestão de Condominios</title>
 
+        <link rel="icon" type="imagem/png" href="{{ url('favicon.png') }}" />
         <!-- Fonts -->
         <link href="https://fonts.googleapis.com/css2?family=Nunito:wght@400;600;700&display=swap" rel="stylesheet">
 
