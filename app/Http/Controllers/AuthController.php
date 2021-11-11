79927f90921976ff42f6d3ce505e24271bc98ead
Diogo Machado <diogo.machado.tgp3@gmail.com>
2021-11-11 15:21:50 +0000

    fix

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -78,7 +78,7 @@ class AuthController extends Controller
                 return view('props.home');
             } elseif ($user->tipo == "adm") {
                 return view('admin.home');
-            } elseif ($user->tipo == "ad_condm") {
+            } elseif ($user->tipo == "adm_condm") {
                 return view('admin_cond.home');
             } 
         }
